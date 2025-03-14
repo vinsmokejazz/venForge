5bbf45fc010f75c0d833526e4eb388e88dc9fc35
vinsmokejazz <vinsmokejazz@gmail.com>
2025-03-15 02:01:44 +0530

    tag

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,22 @@
+const Hero=()=>{
+
+  return(
+    <section className="min-h-screen w-full flex flex-col relative">
+      
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am Venu Prasad...
+          <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">Building Products!</p>
+      </div>
+
+      <div className="w-full h-full absolute inset-0">
+
+      </div>
+
+    </section>
+  ) 
+
+}
+
+export default Hero
