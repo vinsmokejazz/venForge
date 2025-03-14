1bad750b06d683791887fcaed807062dd7db66ca
vinsmokejazz <vinsmokejazz@gmail.com>
2025-03-15 01:23:40 +0530

    finally

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./sections/Navbar"
+
+function App() {
+  return (
+  <main className="max-w-7xl mx-auto">
+    <Navbar/>
+  </main>
+  )
+}
+
+export default App
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar=()=>{
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between items-center py-5 mx-auto ">
+          <a href="/" className="text-neutral-400 font-bold text-xl
+ hover:text-white transition-colors">
+            Venu</a> 
+            <button>
+              <img src="/assets/menu.svg" alt="toggle" className="w-6 h-6"/>
+            </button>
+
+        </div>
+
+      </div>
+    </header>
+  )
+}
+export default Navbar  
